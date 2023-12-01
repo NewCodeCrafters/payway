795c3c2de55ec590fd00345f2f4ab089cea5dca5
officialalkenes <belloabdulhakeemolamide@gmail.com>
2023-12-01 16:08:26 +0100

    chore: create custom user models with user manager

---
+++ user/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+from django.utils.translation import gettext_lazy as _
+
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+from .managers import UserManager
+
+
+class User(PermissionsMixin, AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=12, verbose_name=_("Phone Number"))
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_(
+            "Designates whether the user can has teacher's/staff's Priviledges."
+        ),
+    )
+    is_owner = models.BooleanField(
+        default=False,
+        verbose_name=_("Owner Validity"),
+        help_text=_("Designates whether User is a School Owner"),
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    date_joined = models.DateTimeField(_("Date Joined"), auto_now_add=True)
+    last_login = models.DateTimeField(_("Last Login Date"), auto_now=True)
+
+    verified = models.BooleanField(
+        _("email verification"),
+        default=False,
+        help_text=_("Designates whether this user's email has been Verified. "),
+    )
+
+    USERNAME_FIELD = "email"
+
+    objects = UserManager()
