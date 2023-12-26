a4564e4a09b72e30473da05e28bdc7f946bfec6b
Infinitykay <bekay.asa@icloud.com>
2023-12-26 20:42:13 +0800

    signals

--- profiles/signals.py
+++ profiles/signals.py
@@ -1,1 +1,15 @@
-# create a new account once a currency has been set
+from django.dispatch import receiver
+from django.db.models.signals import pre_save, post_save
+from account.models import Account
+from .models import Profiles
+
+
+@receiver(post_save, sender=Profiles)
+def create_account_signals(sender, instance, created, **kwargs):
+    if created and instance.currency:
+        Account.objects.create(
+            user=instance.user,
+            currency=instance.currency,
+            balance=0,
+            approved=False,
+        )
